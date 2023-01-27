25b1d8c489c35ac21da4090b49b660a3dbdf5678
Jan Lolling <jan.lolling@gmail.com>
2023-01-27 10:22:07 +0100

    Updated for client component

--- src/main/java/de/jlo/talendcomp/sap/DriverManager.java
+++ src/main/java/de/jlo/talendcomp/sap/DriverManager.java
@@ -23,7 +23,7 @@ public class DriverManager {
 		try {
 			Class.forName("com.sap.conn.jco.JCoTable");
 		} catch (ClassNotFoundException e) {
-			throw new Exception("Classes from sapjco3.jar not available. Please take care the sapjco3.jar is placed in the JRE as endorsed lib", e);
+			throw new Exception("Classes from sapjco3.jar not available.", e);
 		}
 		// this is the part we separate the implementation from the interface
 		Driver driver = (Driver) Class.forName("de.jlo.talendcomp.sap.impl.DriverImpl").getDeclaredConstructor().newInstance();
